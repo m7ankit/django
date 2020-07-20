877ccccfce3e6e9461d6be168ab15eb1897f7520
ankit <ankit.lodhi01@gmail.com>
2020-07-21 01:59:17 +0530

    jsfile

--- ecom/ank/views.py
+++ ecom/ank/views.py
@@ -1,55 +1,96 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from django.http import JsonResponse
+import json
 
-# Create your views here.
+
 def home(request):
-	
+    
 
-	context={}
-	context['var1']="this is the line printed directly through views"
+    context={}
+    context['var1']="this is the line printed directly through views"
 
-	context={'var2':'var2 is here through views','var3':'var3 is here '}
+    context={'var2':'var2 is here through views','var3':'var3 is here '}
 
-	list_1=[]
-	list_1.append('hello')
-	list_1.append('how')
-	list_1.append('are ')
-	list_1.append('you.')
+    list_1=[]
+    list_1.append('hello')
+    list_1.append('how')
+    list_1.append('are ')
+    list_1.append('you.')
 
-	context['list_1']=list_1
-	return render(request,'ank/dashboard.html',context)
+    context['list_1']=list_1
+    return render(request,'ank/dashboard.html',context)
 
 def store(request):
-	products = Product.objects.all()
-	context={'products':products}
-	return render(request,'ank/store.html',context)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        #Create empty cart for now for non-logged in user
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+        cartItems = order['get_cart_items']
+
+    products = Product.objects.all()
+    context = {'products':products, 'cartItems':cartItems}
+    return render(request, 'ank/store.html', context)
 
 def cart(request):
-	if request.user.is_authenticated:
-		customer=request.user.customer #this os to set one to one rekationship
-		order,created = Order.objects.get_or_create(customer=customer,complete=False)
-		items = order.orderitem_set.all()
-	else:
-		items=[]
-		items={'get_cart_total':0,'get_items_total':0}
 
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        #Create empty cart for now for non-logged in user
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+        cartItems = order['get_cart_items']
 
-
-	context={'items':items,'order':order}
-	return render(request,'ank/cart.html',context)
+    context = {'items':items, 'order':order, 'cartItems':cartItems}
+    return render(request, 'ank/cart.html', context)
 
 def checkout(request):
-	if request.user.is_authenticated:
-		customer=request.user.customer 
-		order,created = Order.objects.get_or_create(customer=customer,complete=False)
-		items = order.orderitem_set.all()
-	else:
-		items=[]
-		items={'get_cart_total':0,'get_items_total':0}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        #Create empty cart for now for non-logged in user
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+        cartItems = order['get_cart_items']
 
+    context = {'items':items, 'order':order, 'cartItems':cartItems}
+    return render(request, 'ank/checkout.html', context)
 
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    print('Action:', action)
+    print('Product:', productId)
 
-	context={'items':items,'order':order}
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
 
-	return render(request,'ank/checkout.html',context)
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
+    return JsonResponse('Item was added', safe=False)
